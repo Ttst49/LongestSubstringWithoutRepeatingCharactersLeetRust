b748f07405ce82f2005abf23ced0590629a567c4
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-05-26 12:01:38 +0200

    add a way to resolve when there is only one character

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ fn longest_substring(s:String) ->i32{
     let mut longest:i32 = 0;
     let mut actual_string:String = String::from("");
     for char in s.chars() {
+        if s.len() == 1 {
+            longest = 1;
+            actual_string = String::from(char);
+        }
         if char.eq(&' ') {
-            longest += 1
+            longest = 1
         }
         if actual_string.contains(char) {
             if longest < actual_string.len() as i32 {
@@ -16,14 +20,14 @@ fn longest_substring(s:String) ->i32{
                 actual_string = String::from("");
             }
          }else {
-             actual_string.push(char)
+             actual_string.push(char);
          }
     }
     longest
 }
 
 fn main() {
-    let sentence = String::from(" ");
+    let sentence = String::from("au");
     let result = longest_substring(sentence);
     println!("{}",result)
 }
