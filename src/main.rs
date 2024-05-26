2cdd468facb70ba0dc402209283367bb9bd7ddd9
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-05-26 11:29:30 +0200

    first solution resolve 2 out of 3 case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+fn longest_substring(s:String)->i32{
+    let mut longest:i32 = 0;
+    let mut actual_string:String = String::from("");
+    for char in s.chars() {
+         if actual_string.contains(char) {
+             longest = actual_string.len() as i32
+         }else {
+             actual_string.push(char)
+         }
+    }
+    println!("{}",actual_string);
+    longest
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sentence = String::from("pwwkew");
+    let result = longest_substring(sentence);
+    println!("{}",result)
 }
